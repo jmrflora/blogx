ac055e0d588e77d0e89602424cdcb08e4b1f2899
jose matheus <joseribeiroflora@gmail.com>
2024-04-10 22:22:35 -0300

    teste super basicos do restante dos cruds

--- db/crud_test.go
+++ db/crud_test.go
@@ -78,3 +78,75 @@ func TestCreateArtigo(t *testing.T) {
 	assert.NoError(t, err)
 
 }
+
+func TestCreateCategoria(t *testing.T) {
+	db := sqlx.MustConnect("sqlite3", "../mydb.db")
+
+	tx, err := db.Beginx()
+	defer tx.Rollback()
+
+	assert.NoError(t, err)
+
+	c := modelos.CategoriaCreateDTO{
+		NomeCategoria: "nome teste",
+	}
+
+	_, err = CreateCategoria(tx, c)
+
+	assert.NoError(t, err)
+
+}
+
+func TestCreateCategoriasDeArtigo(t *testing.T) {
+	db := sqlx.MustConnect("sqlite3", "../mydb.db")
+
+	tx, err := db.Beginx()
+	defer tx.Rollback()
+
+	assert.NoError(t, err)
+
+	_, err = CreateCategoriasDeArtigo(tx, "63f7e639-2d32-46e7-94a4-328217d81487", 1)
+
+	assert.NoError(t, err)
+}
+
+func TestCategorizarArtigo(t *testing.T) {
+	db := sqlx.MustConnect("sqlite3", "../mydb.db")
+
+	tx, err := db.Beginx()
+	defer tx.Rollback()
+
+	assert.NoError(t, err)
+
+	_, err = CategorizarArtigo(tx, "63f7e639-2d32-46e7-94a4-328217d81487", []int{1})
+
+	assert.NoError(t, err)
+
+}
+
+func TestCreateBlog(t *testing.T) {
+	db := sqlx.MustConnect("sqlite3", "../mydb.db")
+
+	tx, err := db.Beginx()
+	defer tx.Rollback()
+
+	assert.NoError(t, err)
+
+	id := uuid.New()
+	a := modelos.ArtigoCreateDTO{
+		Uuid:      id.String(),
+		Titulo:    "aaaaaaa",
+		Subtitulo: "aaaaaaaaas",
+		IdAutor:   1,
+		Estrelas:  0,
+	}
+
+	b := modelos.BlogCreateDTO{
+		ArtigoCreateDTO: a,
+		CategoriasIds:   []int{1},
+	}
+
+	_, err = CreateBlog(tx, &b)
+
+	assert.NoError(t, err)
+}
