61f1c9265c73c51aaac677b0c360832de645c882
jose matheus <joseribeiroflora@gmail.com>
2024-04-16 21:53:03 -0300

    parando por aqui

--- views/layout/baselayout_templ.go
+++ views/layout/baselayout_templ.go
@@ -76,7 +76,7 @@ func Navbar() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"navbar\"><div class=\"container-fluid\"><a class=\"navbar-brand\" href=\"#\">BLOGX</a></div></nav>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"navbar\"><div class=\"container-fluid\"><a class=\"navbar-brand\" href=\"/\">BLOGX</a></div></nav>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
